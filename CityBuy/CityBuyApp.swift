f1a980615fe833b3670754bba611ef2cc1666adc
steveshi-dotcom <st3v5.s2i@gmail.com>
2022-06-26 10:15:51 -0400

    Initial Commit

---
+++ CityBuy/CityBuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityBuyApp.swift
+//  CityBuy
+//
+//  Created by Steve Shi on 6/26/22.
+//
+
+import SwiftUI
+
+@main
+struct CityBuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
