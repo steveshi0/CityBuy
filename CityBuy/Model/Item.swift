6319f0629566538c465b8019b3293c959e0de2e4
steveshi-dotcom <st3v5.s2i@gmail.com>
2022-07-10 15:25:43 -0400

    git issue with file renaming

---
+++ CityBuy/Model/Item.swift
@@ -0,0 +1,33 @@
+//
+//  Item.swift
+//  CityBuy
+//
+//  Created by Steve Shi on 7/10/22.
+//
+
+import Foundation
+
+// Item Model - individual item
+struct Item: Codable {
+    let name: String
+    let cost: Double
+    let time: String
+    let sold: Bool
+    let image: [Data]
+    let attributes: ItemAttributes
+}
+
+// Enumeration of the categories available for each product
+enum ItemAttributes: String, Codable {
+    case Home = "Home & Kitchen"
+    case Beauty = "Beauty & Personal Care"
+    case Toys = "Toys & Games"
+    case Clothing = "Clothing, Shoes & Jewelry"
+    case Health = "Health, Household & Baby Care"
+    case Sports = "Sports & Outdoors"
+    case Arts = "Arts, Crafts & Sewing"
+    case Books = "Books"
+    case Kitchen = "Kitchen & Dining"
+    case Baby = "Baby"
+    case Electronics = "Electronics"
+}
