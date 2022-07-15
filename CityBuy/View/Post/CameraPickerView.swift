d091821f66b93364a37117eea55533d4c62339a0
steveshi-dotcom <st3v5.s2i@gmail.com>
2022-07-15 08:42:06 -0400

    Uploaded generic camera & photopicker view

---
+++ CityBuy/View/Post/CameraPickerView.swift
@@ -0,0 +1,50 @@
+//
+//  CameraPickerView.swift
+//  CityBuy
+//
+//  Created by Steve Shi on 7/15/22.
+//
+
+import Foundation
+import SwiftUI
+
+// Camera view for user to take pictures of the product
+struct CreationCameraPickerView: UIViewControllerRepresentable {
+    @Binding var isPresented: Bool
+    let onTakePicture: (UIImage) -> Void
+    
+    func makeCoordinator() -> CameraPickerViewCoordinator {
+        return CameraPickerViewCoordinator(isPresented: _isPresented, onTakePicture: onTakePicture)
+    }
+    
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let controller = UIImagePickerController()
+        controller.sourceType = .camera
+        controller.delegate = context.coordinator
+        return controller
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+    }
+    
+}
+
+final class CameraPickerViewCoordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @Binding var isPresented: Bool
+    let onTakePicture: (UIImage) -> Void
+    
+    init(isPresented: Binding<Bool>, onTakePicture: @escaping (UIImage) -> Void) {
+        self._isPresented = isPresented
+        self.onTakePicture = onTakePicture
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage {
+            DispatchQueue.main.async { [weak self] in
+                self?.onTakePicture(image)
+            }
+        }
+        isPresented = false
+    }
+}
---
+++ CityBuy/View/Post/PhotoPickerView.swift
@@ -0,0 +1,49 @@
+//
+//  PhotoPickerView.swift
+//  CityBuy
+//
+//  Created by Steve Shi on 7/15/22.
+//
+
+import SwiftUI
+import PhotosUI
+
+// Photo picker view to allow the user to pick images from their album
+struct CreationPhotosPickerView: UIViewControllerRepresentable {
+    @Binding var isPresented: Bool
+    let onResultsHandler: ([PHPickerResult]) -> Void
+    
+    func makeCoordinator() -> PhotosPickerViewCoordinator {
+        PhotosPickerViewCoordinator(isPresented: _isPresented, onResultsHandler: onResultsHandler)
+    }
+    
+    func makeUIViewController(context: Context) -> PHPickerViewController {
+        var configuration = PHPickerConfiguration(photoLibrary: .shared())
+        configuration.filter = .any(of: [.images])
+        configuration.selectionLimit = 4
+        configuration.selection = .ordered
+        let controller = PHPickerViewController(configuration: configuration)
+        controller.delegate = context.coordinator
+        return controller
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
+    }
+}
+
+final class PhotosPickerViewCoordinator: PHPickerViewControllerDelegate {
+    let onResultsHandler: ([PHPickerResult]) -> Void
+    @Binding var isPresented: Bool
+    
+    init(isPresented: Binding<Bool>, onResultsHandler: @escaping ([PHPickerResult]) -> Void) {
+        self._isPresented = isPresented
+        self.onResultsHandler = onResultsHandler
+    }
+    
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        let imageResults = results.filter { $0.itemProvider.canLoadObject(ofClass: UIImage.self) }
+        onResultsHandler(imageResults)
+        isPresented = false
+    }
+}
